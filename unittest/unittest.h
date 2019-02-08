8ac4e31c5c0a1427e8563e7006d0d7fd2afef081
Gioh Kim <gurugio@hanmail.net>
2019-02-08 22:12:05 +0100

    DEFINE_UNITTEST is wrapper of register_test

--- unittest/unittest.h
+++ unittest/unittest.h
@@ -14,11 +14,13 @@ struct unittest_set {
 	struct unittest *tests[SET_MAX];
 };
 
-#define DEFINE_UNITTEST(__name) \
-	void register_##__name(void)
+#define DEFINE_UNITTEST(__name, __struct_unittest)	\
+	void register_##__name(void) {					\
+		register_test(#__name, &__struct_unittest);	\
+	}
 
 #define REGISTER_UNITTEST(__name)    \
-	void register_##__name(void); \
+	void register_##__name(void);	 \
 	register_##__name()
 
 int register_test(char *name, struct unittest *test);
